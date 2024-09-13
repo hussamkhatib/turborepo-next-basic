8a9e590dccbbd56fae6320824ac1f9fd4ec445b8
hussamkhatib <hussamkhatib20@gmail.com>
2024-09-14 02:20:55 +0400

    Pull vc

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -5,6 +5,7 @@ module.exports = {
   transpilePackages: ["@repo/ui"],
 };
 
+console.log(process.env.NEXT_PUBLIC_APP_NAME,"BROOOO")
 if(!process.env.NEXT_PUBLIC_APP_NAME) {
   throw new Error('NEXT_PUBLIC_APP_NAME is not set')
 }
